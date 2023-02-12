e2ecb06ee5da92510bb13cdbb5fa4565f0fe6456
Marshmillo <jcamilodz@gmail.com>
2023-02-12 18:43:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import Container from './components/Container';
+import Header from './components/Header'
+import InputTask from './components/InputTask';
+import {useState, useEffect} from 'react'
+import TaskContent from './components/TaskContent';
 
 function App() {
+  let initialTasks = JSON.parse(localStorage.getItem('tasks'));
+
+  if(!initialTasks){
+    initialTasks = [];
+  }
+
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const createTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (id) => {
+    const currentTasks = tasks.filter((task) => task.idTask !== id);
+    setTasks(currentTasks);
+  }
+
+  useEffect(() => {
+    if(initialTasks){
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      localStorage.setItem('tasks', JSON.stringify([]));
+    }
+  }, [initialTasks, tasks]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header/>
+      <InputTask createTask={createTask}/>
+      <TaskContent tasks={tasks} deleteTask={deleteTask}></TaskContent>
+    </Container>
   );
 }
 
---
+++ src/components/InputTask.js
@@ -0,0 +1,68 @@
+import {Select, Input, Button, Grid, Header, Icon} from 'semantic-ui-react';
+import {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const options = [
+    { key: 'deportes', text: 'Deporte', value: 'deporte'},
+    { key: 'casa', text: 'Casa', value: 'casa'},
+    { key: 'oficina', text: 'Oficina', value: 'oficina'},
+    { key: 'otra', text: 'Otra', value: 'otra'},
+]
+export default function InputTask(props) {
+    const {createTask} = props;
+    const [task, setTask] = useState({
+        idTask: '',
+        taskName: '',
+        categoryTask: ''
+    });
+    const [error, setError] = useState(false);
+    const onChangeTask = (e) => {
+        setTask({
+            ...task,
+            [e.target.name]: e.target.value
+        });
+    };
+    const onChangeCategoryTask = (e, data) => {
+        setTask({
+            ...task,
+            [data.name]: data.value
+        });
+    };
+    const onSubmitTask = (e) => {
+        e.preventDefault();
+        if(task.taskName.trim() === ""){
+            setError();
+            return;
+        }
+        setError(false);
+        task.idTask = uuidv4();
+        createTask(task);
+        setTask({
+            idTask: '',
+            taskName: '',
+            categoryTask: ''
+        });
+        
+    };
+
+    return (
+        <>
+            <Grid centered columns={2}>
+                <Input type='text'action>
+                    <Input size='small' icon='add' placeholder='Escribe tu tarea...' iconPosition='left' name='taskName' value={task.taskName} onChange={onChangeTask}/>
+                    <Select compact options={options} className='select-form-task' name='categoryTask' placeholder='Categoria' value={task.categoryTask} onChange={onChangeCategoryTask}/>
+                    <Button type='submit' color='violet' onClick={onSubmitTask}>Añadir tarea</Button>
+                </Input>
+            </Grid>
+            {error && (
+                <Grid>
+                    <Header as='h4' color='red' className='alert-error-form'>
+                        <Icon name='close'/>
+                        <Header.Content>La task es obligatoria</Header.Content>
+                        <Icon name='close'/>
+                    </Header>
+                </Grid>
+            )}
+        </>
+    );
+}
